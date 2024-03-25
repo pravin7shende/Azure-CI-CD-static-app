f0ae686f12806495462833c0cb6449fb885efc26
pravin7shende <pravin7shende@gmail.com>
2024-03-26 04:11:12 +0530

    fixed AB#4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Your LLM Model</h1>
-	<h2>Upload your data to make to get predictions</h2>
+	<h2>Congratulations Pravin ! Now Upload your data to make to get predictions</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
